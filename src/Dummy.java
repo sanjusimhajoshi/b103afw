633d6d315799429a27ad7062350f2e93caffaa71
ssimha <sanju.simha@xperi.com>
2024-07-09 08:09:06 +0530

    Updated

--- src/Dummy.java
+++ src/Dummy.java
@@ -1,4 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Dummy {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("file:///E:/B103_Selenium/share/pages/sample4.html");
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Bhanu");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("ABC");
+
+	}
 
 }
